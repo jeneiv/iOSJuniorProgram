db5fb0c506a4f88f18db47b92ea81b1a986a49d9
Jenei Viktor <jeneiv@gmail.com>
2014-02-24 13:03:24 +0100

    Base revision (empty iOS project) added

---
+++ iOSJuniorApplication/iOSJuniorApplication/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOSJuniorApplication
+//
+//  Created by Jenei Viktor on 24/02/14.
+//  Copyright (c) 2014 victo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOSJuniorApplication/iOSJuniorApplicationTests/iOSJuniorApplicationTests.m
@@ -0,0 +1,34 @@
+//
+//  iOSJuniorApplicationTests.m
+//  iOSJuniorApplicationTests
+//
+//  Created by Jenei Viktor on 24/02/14.
+//  Copyright (c) 2014 victo. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface iOSJuniorApplicationTests : XCTestCase
+
+@end
+
+@implementation iOSJuniorApplicationTests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
